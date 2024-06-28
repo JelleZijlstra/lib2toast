c2de5357020b51f43c3a15f74aabd9383759fc85
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-06-28 16:20:53 -0700

    Check in integration test

---
+++ tests/test_integration.py
@@ -0,0 +1,22 @@
+from collections.abc import Iterable
+from pathlib import Path
+
+import lib2toast
+import pytest
+
+from .test_compile import assert_compiles
+
+
+def generate_test_cases() -> Iterable[Path]:
+    test_dir = Path(__file__).parent
+    for file_path in test_dir.glob("*.py"):
+        yield file_path
+    lib2toast_dir = Path(lib2toast.__file__).parent
+    for file_path in lib2toast_dir.glob("*.py"):
+        yield file_path
+
+
+@pytest.mark.parametrize("file_path", generate_test_cases())
+def test_python_file(file_path: Path) -> None:
+    code = file_path.read_text()
+    assert_compiles(code)
