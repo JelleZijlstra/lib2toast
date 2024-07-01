bbf0fbb0edcd88cea2353ee76b9882e3ff6cdadd
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-07-01 11:06:50 -0700

    More tests, fixes

--- tests/test_custom_grammar.py
+++ tests/test_custom_grammar.py
@@ -1,13 +1,18 @@
 import ast
 import textwrap
+from collections.abc import Sequence
 from pathlib import Path
 from typing import Any
 
+from blib2to3 import pygram
+from blib2to3.pgen2 import token
 from blib2to3.pgen2.grammar import Grammar
-from blib2to3.pgen2.token import NL
+from blib2to3.pytree import NL, Node
 from lib2toast.api import load_grammar
 from lib2toast.compile import Compiler, Consumer, LineRange, get_line_range
 from lib2toast.run import run
+
+syms = pygram.python_symbols
 
 
 def check_run(
@@ -25,8 +30,30 @@ class BracesCompiler(Compiler):
         return self.compile_suite(consumer.expect())
 
     def compile_suite(self, node: NL) -> tuple[list[ast.stmt], LineRange]:
-        statements = self.compile_statement_list([node.children[1]])
+        consumer = Consumer(node.children)
+        statement_list = []
+        consumer.consume(token.LBRACE)
+        while not consumer.next_is(token.RBRACE):
+            statement_list.append(consumer.expect())
+        statements = self.compile_statement_list(statement_list)
         return statements, get_line_range(node, ignore_last_leaf=True)
+
+    def compile_statement_list(self, nodes: Sequence[NL]) -> list[ast.stmt]:
+        statements = []
+        for node in nodes:
+            if node.type in (token.ENDMARKER, token.NEWLINE):
+                continue
+            if isinstance(node, Node) and node.type == syms.simple_stmt:
+                statements += self.compile_simple_stmt(node)
+            else:
+                stmt = self.visit(node)
+                if isinstance(stmt, ast.stmt):
+                    statements.append(stmt)
+                elif isinstance(stmt, ast.expr):
+                    statements.append(ast.Expr(stmt, **get_line_range(node)))
+                else:
+                    raise AssertionError(f"Unexpected statement: {stmt}")
+        return statements
 
 
 def test_braces() -> None:
@@ -42,6 +69,40 @@ def test_braces() -> None:
         }
         """,
         {"x": 1},
+        grammar=grammar,
+        compiler=BracesCompiler(),
+    )
+    check_run(
+        """
+        def f(x) {
+            return x + 1
+        }
+        lst = []
+        for i in range(4) {
+            lst.append(f(i))
+        }
+        """,
+        {"lst": [1, 2, 3, 4]},
+        grammar=grammar,
+        compiler=BracesCompiler(),
+    )
+    check_run(
+        """
+        class C {
+            def meth(self) {
+                self.x = 1
+            }
+            def meth2(self, x) {
+                self.x = x
+            }
+        }
+        c = C()
+        c.meth()
+        x1 = c.x
+        c.meth2(2)
+        x2 = c.x
+        """,
+        {"x1": 1, "x2": 2},
         grammar=grammar,
         compiler=BracesCompiler(),
     )
